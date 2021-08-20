b55bab3d64c6900a71d5b1a1ad135045e1577b06
ekspant <73355369+ekspant@users.noreply.github.com>
2021-08-20 16:00:01 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,6 +3,13 @@ $(document).ready(function () {
   var floorPath = $('.home-image path'); //каждый отдельный этаж в svg
   var counterUp = $('.counter-up'); //кнопка увеличения этажа
   var counterDown = $('.counter-down'); //кнопка уменьшения этажа
+  var modal = $('.modal');
+  var modelCloseButton = $('.modal-close-button');
+  var viewFlatsButton = $('.view-flats');
+  
+  var flatPath =$('.flats path'); //каждая отдельная квартира на этаже в SVG
+  var currentFlat =0;//переменная хранящая номер квартиры
+
 
   //Выделяется этаж при наведении мышкой
   floorPath.on('mouseover', function () {
@@ -10,6 +17,10 @@ $(document).ready(function () {
     currentFloor = $(this).attr('data-floor'); //получаем значение текущего этажа
     $('.counter').text(currentFloor); //записываем значения этажа в счетчик справа
   });
+
+  floorPath.on('click', toggleModal); //при клике на этаж открыть модальное окно
+  modelCloseButton.on('click', toggleModal); //при клике на кнопку закрыть - закрыть модальное окно
+  viewFlatsButton.on('click', toggleModal); //открытие модального окна по нажатию кнопки
 
   //увеличение этажа и его выделение по кнопке вверх
   counterUp.on('click', function () {
@@ -36,4 +47,19 @@ $(document).ready(function () {
       $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor'); //подсвечиваем текущий этаж
     }
   });
+
+  //функция закрытия-открытия модального окна
+  function toggleModal() {
+    modal.toggleClass('is-open');
+  };
+
+   //Выделяется КВАРТИРА при наведении мышкой
+    flatPath.on('mouseover', function () {
+    //flatPath.removeClass('current-flat'); //удаляем активный класс (у выделенного этажа)
+    currentFlat = $(this).attr('data-flat'); //получаем значение текущей квартиры
+    $('.counter').text(currentFlat); //записываем значения этажа в счетчик справа
+    
+  });
+
+
 });
